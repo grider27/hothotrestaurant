e39d6f72f1414df6ba6645fdfbd43059211b4317
Gary B <grider27@gmail.com>
2021-03-06 12:03:34 -0500

    move html files to public folder and update html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,14 +3,14 @@ var path = require('path');
 module.exports = function(app){
 
 	app.get('/tables', function(req, res){
-		res.sendFile(path.join(__dirname + '/../tables.html'));
+		res.sendFile(path.join(__dirname + '/../public/tables.html'));
 	});
 
 	app.get('/reserve', function(req, res){
-		res.sendFile(path.join(__dirname + '/../reservation.html'));
+		res.sendFile(path.join(__dirname + '/../public/reservation.html'));
 	});
 
 	app.use(function(req, res){
-		res.sendFile(path.join(__dirname + '/../index.html'));
+		res.sendFile(path.join(__dirname + '/../public/index.html'));
 	});
 }
