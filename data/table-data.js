2cd082a0daad7dc3b2f35e04479208601f78f38e
Brian Flieck <brianflieck@gmail.com>
2021-03-06 11:23:17 -0500

    updated api-routes, data/table-data.js data/waitinglist-data.js

---
+++ data/table-data.js
@@ -0,0 +1,11 @@
+var tableArray = [
+	{
+		customerName: "Brian",
+		customerEmail: "zippzap@hotmail.com",
+		customerID: "007",
+		phoneNumber: "610-222-3456",
+
+	}
+];
+
+module.exports = tableArray; 
