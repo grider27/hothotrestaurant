f3b9227b5abf1ac34f4637d2fa2e9a940ce082c6
Gopal Patel <patelgopalk1@gmail.com>
2021-03-06 13:36:46 -0500

    able to submit reservation

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,11 +2,11 @@ var path = require('path');
 
 module.exports = function(app){
 
-	app.get('/tables', function(req, res){
+	app.get('/tables.html', function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/tables.html'));
 	});
 
-	app.get('/reserve', function(req, res){
+	app.get('/reservation.html', function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/reservation.html'));
 	});
 
